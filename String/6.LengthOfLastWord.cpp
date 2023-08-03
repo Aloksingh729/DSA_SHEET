fbaf100fefc3908c360936aa5a784900749a5cea
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2023-08-03 23:13:05 +0530

    Update 6.LengthOfLastWord.cpp

--- String/6.LengthOfLastWord.cpp
+++ String/6.LengthOfLastWord.cpp
@@ -1,1 +1,19 @@
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
 
+Code:
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size() , cnt =0 ,flag=0;
+       for(int i=n-1;i>=0;i--){
+           if(s[i]==' '&&flag)break;
+           if(s[i]!=' '){
+               flag=1;
+               cnt++;
+           }
+       }
+       return cnt;
+    }
+};
