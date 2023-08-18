39fb516bb9b730eb235b273edf98b4f01e1dda24
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2023-08-18 10:46:57 +0530

    Time: 5 ms (36.28%) | Memory: 7.6 MB (92.36%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        int dp[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0; // If there's an obstacle, no paths are possible
+                } else {
+                    if (i == 0 && j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        int up = (i > 0) ? dp[i - 1][j] : 0;
+                        int left = (j > 0) ? dp[i][j - 1] : 0;
+                        dp[i][j] = up + left;
+                    }
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
