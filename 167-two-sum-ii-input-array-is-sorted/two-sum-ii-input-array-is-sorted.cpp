76ec975718e394fbf771e30687565d9fa89e3e3f
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2023-08-20 01:06:20 +0530

    Time: 11 ms (72.71%) | Memory: 15.6 MB (69.14%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,33 +1,13 @@
-class Solution
-{
-    public:
-int binarySearch(const vector<int>& numbers, int target, int start) {
-    int left = start, right = numbers.size() - 1;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (numbers[mid] == target) {
-            return mid;
-        } else if (numbers[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    
-    return -1;
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0; int right = numbers.size()-1;
 
-vector<int> twoSum(const vector<int>& numbers, int target) {
-    for (int i = 0; i < numbers.size(); ++i) {
-        int complement = target - numbers[i];
-        int index = binarySearch(numbers, complement, i + 1);
-        if (index != -1) {
-            return {i + 1, index + 1};
+        while (left < right){
+            if (numbers[left]+numbers[right] == target) return {left+1, right+1};
+            else if (numbers[left]+numbers[right] > target) --right;
+            else ++left;
         }
+        return {-1, -1};
     }
-    
-    return {}; // No solution found, should not happen according to problem statement
-}
-
 };
