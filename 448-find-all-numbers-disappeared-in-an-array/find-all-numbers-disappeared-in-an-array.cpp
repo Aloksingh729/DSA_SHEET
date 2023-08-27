b8a485b59f3ef2ea111f45b4ab0c4b9bba53eed2
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2023-08-28 01:14:47 +0530

    Time: 83 ms (35.43%) | Memory: 33.8 MB (60.07%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+        sort(begin(nums), end(nums));
         vector<int> ans;
-        
-        unordered_set<int> numSet;
-        
-        // Add all numbers from the array to the hash set
-        for (int num : nums) {
-            numSet.insert(num);
-        }
-        
-        // Check for missing numbers in the range [1, n]
-        for (int i = 1; i <= nums.size(); i++) {
-            if (numSet.find(i) == numSet.end()) {
+        for(int i = 1; i <= size(nums); i++) 
+            if(!binary_search(begin(nums), end(nums), i))   // binary search in nums for each i
                 ans.push_back(i);
-            }
-        }
-        
         return ans;
     }
 };
