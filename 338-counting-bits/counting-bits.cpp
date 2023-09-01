fca092c02017f0418b269db351df797fcd69ca21
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2023-09-01 22:46:51 +0530

    Time: 3 ms (89.82%) | Memory: 7.9 MB (65.80%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -4,18 +4,18 @@ public:
         vector<int> res(n + 1); // Create a result vector of size n + 1
         
         for (int i = 0; i <= n; i++) {
-            res[i] = solve(i);
+            res[i] = solve(i,res);
         }
         
         return res;
     }
     
-    int solve(int n) {
+    int solve(int n, vector<int>&res) {
         // Base conditions
         if (n == 0) return 0;
         if (n == 1) return 1;
-        
-        if (n % 2 == 0) return solve(n / 2); // Handling even case
-        else return 1 + solve(n / 2); // Handling odd case
+         if(res[n]!=0) return res[n];
+        if (n % 2 == 0) return solve(n / 2,res); // Handling even case
+        else return 1 + solve(n / 2,res); // Handling odd case
     }
 };
