6cc1b28f45fac328733f826c788715aa6f42a2e4
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2023-09-10 00:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (46.24%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,17 +1,19 @@
-class Solution {
-public:
-    int combinationSum4(std::vector<int>& nums, int target) {
-        std::vector<unsigned int> dp(target + 1, 0);
-        dp[0] = 1;
-        
-        for (int i = 1; i <= target; ++i) {
-            for (int num : nums) {
-                if (i - num >= 0) {
-                    dp[i] += dp[i - num];
-                }
-            }
+class Solution
+{
+    public:
+        int combinationSum4(vector<int> &nums, int target)
+        {
+            vector<int> dp(target + 1, -1);
+            return solve(nums, target, dp);
         }
-        
+    int solve(vector<int> &nums, int target, vector<int> &dp)
+    {
+        if (target == 0) return 1;
+        if (dp[target] != -1) return dp[target];
+        dp[target] = 0;
+        for (auto &num: nums)
+            if (num <= target)
+                dp[target] += solve(nums, target - num, dp);
         return dp[target];
     }
 };
