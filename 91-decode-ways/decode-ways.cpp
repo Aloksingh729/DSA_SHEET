bb1b7f7d9568d1e110d55fc9c4823336ad3ee6a3
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2023-12-26 00:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (44.81%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+   int solve(string &s,int n){
+       vector<int> t(n+1,0);
+        t[n]=1;
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0')
+            t[i]=0;
+            else{
+                t[i]=t[i+1];
+                if(i<n-1 && (s[i]=='1' || (s[i]=='2'&& s[i+1]<'7')))
+                t[i]+=t[i+2];
+            }
+        }
+        return t[0];
+   }
+
+
+
+
+
+    int numDecodings(string s) {
+        int n=s.length();
+        vector<int> t(n+1,-1);
+        return solve(s,n);
+    }
+};
