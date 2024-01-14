b1f0e043209b4a2a97a23ea7e2402d135af7b8a9
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2024-01-15 01:41:56 +0530

    Time: 61 ms (95.70%) | Memory: 20.9 MB (89.09%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-    // Step 1
-        if (word1.length() != word2.length()) {
-            return false;
-        }
+    
+        // if (word1.length() != word2.length()) {
+        //     return false;
+        // }
 
-        // Step 2
+        
         vector<int> freq1(26, 0), freq2(26, 0);
         for (char c : word1) {
             freq1[c - 'a']++;
@@ -14,17 +14,20 @@ public:
         for (char c : word2) {
             freq2[c - 'a']++;
         }
-
-        // Step 3
+              for(int i = 0; i<26; i++) {
+            if(freq1[i] != 0 && freq2[i] != 0) continue;
+            
+            if(freq1[i] == 0 && freq2[i] == 0) continue;
+            
+            return false;
+        }
+        
         sort(freq1.begin(), freq1.end());
         sort(freq2.begin(), freq2.end());
 
-        // Step 4
-        if (freq1 != freq2) {
-            return false;
-        }
-
-        // Step 5
-        return set<char>(word1.begin(), word1.end()) == set<char>(word2.begin(), word2.end());
+    
+     
+          return freq1==freq2;
+     
     }
 };
