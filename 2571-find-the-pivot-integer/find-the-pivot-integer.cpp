b74e310456b4d3dd7f379f2619dc6c5a34e9a67a
Alok singh <86466969+Aloksingh729@users.noreply.github.com>
2024-03-14 01:33:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (74.39%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        if(n == 1)
+            return 1;
+        
+        int totalSum = n * (n+1)/2;
+        
+        int pivot = sqrt(totalSum);
+        
+        if(pivot*pivot == totalSum)
+            return pivot;
+        
+        return -1;
+        
+    }
+};
